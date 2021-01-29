e2d094302bc8b6c732b1128a8d021a09293408f9
przekwas <luke@covalence.io>
2021-01-29 01:23:23 -0600

    fixing old import statement

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,9 +1,9 @@
-import { IUser } from '../../interfaces';
+import { UserModel } from '../../interfaces';
 
 declare global {
   namespace Express {
     export interface Request {
-      currentUser: IUser;
+      currentUser: UserModel;
     }    
   }
 }
